0e8fb474c37a40813eb14851cab608e60efdeb4a
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-07-29 22:05:14 +0600

    Another simple module for practice

---
+++ example-module/math.js
@@ -0,0 +1,4 @@
+exports.sum = (n1, n2) => n1 + n2;
+exports.div = (n1, n2) => n1 / n2;
+exports.mul = (n1, n2) => n1 * n2;
+exports.sub = (n1, n2) => n1 - n2;
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const rect = require('./example-module/rectangle');
+const math = require('./example-module/math');
 
 function solveRect(l,b) {
     console.log("Solving for rectangle with l = " + l + " and b = " + b);
@@ -17,3 +18,8 @@ solveRect(2,4);
 solveRect(3,5);
 solveRect(0,5);
 solveRect(-3,5);
+
+console.log(`addition of 2, 4 is ${math.sum(2,4)}`);
+console.log(`substruct of 2, 4 is ${math.sub(2,4)}`);
+console.log(`division of 2, 4 is ${math.div(2,4)}`);
+console.log(`multiplication of 2, 4 is ${math.mul(2,4)}`);
