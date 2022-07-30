e6e4371f9cd760347afedb465e02c56e2dd574af
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-07-30 12:44:15 +0600

    Practice node callback

--- example-module/rectangle.js
+++ example-module/rectangle.js
@@ -1,2 +1,22 @@
-exports.area = (x, y) => (x * y);
-exports.perimeter = (x, y) => (2 * (x + y));
+module.exports = (x, y, callback) => {
+  if (x <= 0 || y <= 0) {
+    setTimeout(() => {
+      callback(
+        new Error(
+          "Rectangle dimensions should be greater than zero:  l = " +
+            x +
+            ",  and b = " +
+            y
+        ),
+        null
+      );
+    }, 2000);
+  } else {
+    setTimeout(() => {
+      callback(null, {
+        area: () => x * y,
+        perimeter: () => 2 * (x + y),
+      });
+    }, 2000);
+  }
+};
--- index.js
+++ index.js
@@ -4,14 +4,17 @@ const math = require('./example-module/math');
 function solveRect(l,b) {
     console.log("Solving for rectangle with l = " + l + " and b = " + b);
 
-    if (l <= 0 || b <= 0) {
-        console.log("Rectangle dimensions should be greater than zero:  l = "
-               + l + ",  and b = " + b);
-    }
-    else {
-	    console.log("The area of the rectangle is " + rect.area(l,b));
-	    console.log("The perimeter of the rectangle is " + rect.perimeter(l,b));
-    }
+        rect(l, b, (err, rect) => {
+            if (err) {
+                console.log(`Err is ${err.message}`);
+            } else {
+                console.log("The area of the rectangle is " + rect.area());
+                console.log(
+                  "The perimeter of the rectangle is " + rect.perimeter(l, b)
+                );
+            }
+
+        });
 }
 
 solveRect(2,4);
