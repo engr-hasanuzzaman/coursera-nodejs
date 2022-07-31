921c7de6596ae72957e13202f9ec373200b1b509
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-07-31 10:55:53 +0600

    Create express route for dishes

--- node-express/index.js
+++ node-express/index.js
@@ -1,15 +1,34 @@
+/*---------------
+* import
+* ---------------*/ 
 const express = require('express');
-const app = express();
-const host = 'localhost';
 const fs = require('fs');
 const path = require('path');
 const morgan = require('morgan');
+const dishRouter = require('./routes/dishRouter');
 
+/*---------------
+* constant
+* ---------------*/ 
+const app = express();
+const host = 'localhost';
 const _404Path = path.resolve(__dirname, './public/404.html');
 const port = 3000;
+
+/*---------------
+* middlewares
+* ---------------*/
 app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
 
+/*---------------
+* routes
+* ---------------*/
+app.use('/dishes', dishRouter);
+
+/*---------------
+* others
+* ---------------*/
 app.get('*', (_, res) => {
     fs.createReadStream(_404Path).pipe(res);
 })
